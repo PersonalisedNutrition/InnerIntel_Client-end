507516705e399ed85bc7e91e64061520d5bf3012
ShiyunZhu <u7041419@anu.edu.au>
2022-04-25 19:45:21 +0800

    add package activity

---
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/LoginActivity.java
@@ -0,0 +1,8 @@
+package au.edu.anu.cecs.innerintel.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/WelcomeActivity.java
@@ -0,0 +1,6 @@
+package au.edu.anu.cecs.innerintel.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+}

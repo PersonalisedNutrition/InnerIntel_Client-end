021935e867eef5512302fbbf947d364278873474
ShiyunZhu <u7041419@anu.edu.au>
2022-04-29 00:17:04 +0800

    change  name of image

--- app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/RegisterActivity.java
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/RegisterActivity.java
@@ -130,18 +130,7 @@ public class RegisterActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
+
 
                         // ...
                     }

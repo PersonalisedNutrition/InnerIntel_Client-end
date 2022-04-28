c7670b37a7df6b2507af14fc49ec0544864ae5e4
Shuyi Chen <u7175274@anu.edu.au>
2022-04-28 20:56:53 +0800

    Add Unit and MealFood class

---
+++ app/src/main/java/au/edu/anu/cecs/innerintel/Bean/enumType/Unit.java
@@ -0,0 +1,18 @@
+package au.edu.anu.cecs.innerintel.Bean.enumType;
+
+/**
+ * Enumerate food unit
+ *
+ * @author Shuyi Chen
+ */
+public enum Unit {
+    G,
+    GRAIN,
+    DRACHM,
+    OUNCE,
+    POUND,
+    ML,
+    GILL,
+    PINT,
+    QUART
+}

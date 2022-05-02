20bc552c39fcabae5e55f3999e947dfc309bf5cb
MercyChen <mercychen02@gmail.com>
2022-05-02 20:02:40 +0800

    Add Dropdown, DatePicker function Add mainColor, left_right margin

--- app/src/main/java/au/edu/anu/cecs/innerintel/activity/AddMealActivity.java
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/AddMealActivity.java
@@ -2,8 +2,22 @@ package au.edu.anu.cecs.innerintel.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import au.edu.anu.cecs.innerintel.Bean.User;
 import au.edu.anu.cecs.innerintel.Bean.enumType.LogType;
@@ -11,10 +25,18 @@ import au.edu.anu.cecs.innerintel.Bean.enumType.MealType;
 import au.edu.anu.cecs.innerintel.R;
 
 public class AddMealActivity extends AppCompatActivity {
-    User loginUser;
-    LogType logType;
-    MealType mealType;
-    Spinner spiMealType;
+    public User loginUser;
+    public LogType logType;
+    public Spinner spiMealType;
+    public Button btnDate;
+    public Button btnTime;
+    public TextView tvDate;
+    public DatePickerDialog datePickerDialog;
+    public TimePickerDialog timePickerDialog;
+    public Calendar calendar;
+
+    private List<MealType> mealTypeList = null;
+    private ArrayAdapter<MealType> mealTypeAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +46,51 @@ public class AddMealActivity extends AppCompatActivity {
         loginUser = (User) getIntent().getExtras().getSerializable("user");
         logType = (LogType) getIntent().getExtras().getSerializable("logtype");
 
+        calendar = Calendar.getInstance();
+
+        btnDate = findViewById(R.id.addmeal_btn_time);
+        btnTime = findViewById(R.id.addmeal_btn_date);
+        tvDate = findViewById(R.id.addmeal_tv_date);
+
+        btnDate.setOnClickListener(allListener);
+        btnTime.setOnClickListener(allListener);
+
         spiMealType = findViewById(R.id.addmeal_spi_mealtype);
-        spiMealType.setPrompt("");
+        spiMealType.setPrompt("Which meal would you like to add?");
+        mealTypeList = new ArrayList<>();
+        mealTypeList.add(MealType.BREAKFAST);
+        mealTypeList.add(MealType.LUNCH);
+        mealTypeList.add(MealType.DINNER);
+        mealTypeList.add(MealType.OTHERS);
+        mealTypeAdapter = new ArrayAdapter<MealType>(this, R.layout.support_simple_spinner_dropdown_item, mealTypeList);
+        spiMealType.setAdapter(mealTypeAdapter);
+    }
+
+    private View.OnClickListener allListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.addmeal_btn_date:
+                    showCalendarDialog();
+                    break;
+                case R.id.addmeal_btn_time:
+//                    showCalenderDialog();
+                    break;
+                case R.id.addmeal_btn_addfood:
+                    break;
+            }
+        }
+    };
+
+    private void showCalendarDialog() {
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String calendar = year + "/" + (month + 1) + "/" + day;
+                tvDate.setText(calendar);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+        datePickerDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }

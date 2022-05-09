2e71f9c22aa8c2d10c4a69689dadd458e575c1f4
MercyChen <mercychen02@gmail.com>
2022-05-10 00:20:43 +0800

    Optimize Message Page

---
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/MessageActivity.java
@@ -0,0 +1,16 @@
+package au.edu.anu.cecs.innerintel.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import au.edu.anu.cecs.innerintel.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}

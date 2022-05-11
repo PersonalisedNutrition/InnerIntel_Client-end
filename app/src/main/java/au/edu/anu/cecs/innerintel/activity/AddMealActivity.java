dca331a5abc8f621b6799cbb57818c89c1d63ff1
MercyChen <mercychen02@gmail.com>
2022-05-11 21:40:07 +0800

    Add Function UI -> Label a meal

--- app/src/main/java/au/edu/anu/cecs/innerintel/activity/AddMealActivity.java
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/AddMealActivity.java
@@ -2,8 +2,10 @@ package au.edu.anu.cecs.innerintel.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +13,12 @@ import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -24,6 +27,7 @@ import au.edu.anu.cecs.innerintel.Bean.User;
 import au.edu.anu.cecs.innerintel.Bean.enumType.LogType;
 import au.edu.anu.cecs.innerintel.Bean.enumType.MealType;
 import au.edu.anu.cecs.innerintel.R;
+
 /**
  * Interact the add meal page
  *
@@ -36,11 +40,17 @@ public class AddMealActivity extends AppCompatActivity {
     public Button btnDate;
     public Button btnTime;
     public Button btnAddFood;
+    public Button btnLoadRecipe;
+    public Button btnAddMeal;
+    public Button btnAddRecipe;
     public TextView tvDate;
     public TextView tvTime;
+
     public DatePickerDialog datePickerDialog;
     public TimePickerDialog timePickerDialog;
     public Calendar calendar;
+    public AlertDialog alertDialog;
+    public AlertDialog alertDialogIn;
 
     private List<MealType> mealTypeList = null;
     private ArrayAdapter<MealType> mealTypeAdapter = null;
@@ -58,12 +68,19 @@ public class AddMealActivity extends AppCompatActivity {
         btnDate = findViewById(R.id.addmeal_btn_time);
         btnTime = findViewById(R.id.addmeal_btn_date);
         btnAddFood = findViewById(R.id.addmeal_btn_addfood);
+        btnLoadRecipe = findViewById(R.id.addmeal_btn_loadrecipe);
+        btnAddRecipe = findViewById(R.id.addmeal_btn_addrecipe);
+        btnAddMeal = findViewById(R.id.addmeal_btn_addmeal);
+
         tvDate = findViewById(R.id.addmeal_tv_date);
         tvTime = findViewById(R.id.addmeal_tv_time);
 
         btnDate.setOnClickListener(allListener);
         btnTime.setOnClickListener(allListener);
         btnAddFood.setOnClickListener(allListener);
+        btnLoadRecipe.setOnClickListener(allListener);
+        btnAddRecipe.setOnClickListener(allListener);
+        btnAddMeal.setOnClickListener(allListener);
 
         spiMealType = findViewById(R.id.addmeal_spi_mealtype);
         spiMealType.setPrompt("Which meal would you like to add?");
@@ -92,6 +109,48 @@ public class AddMealActivity extends AppCompatActivity {
 //                    intent.putExtra("user", (Serializable) loginUser);
 //                    intent.putExtra("logtype", logType);
                     startActivity(intent);
+                    break;
+                case R.id.addmeal_btn_loadrecipe:
+                    break;
+                case R.id.addmeal_btn_addrecipe:
+                    alertDialog = new AlertDialog.Builder(AddMealActivity.this)
+                            .setTitle("New Recipe")
+                            .setMessage("Do you want to add this meal as a new recipe?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    View view = getLayoutInflater().inflate(R.layout.dialogedittext, null);
+                                    final EditText editText = view.findViewById(R.id.dialog_edittext);
+                                    alertDialogIn = new AlertDialog.Builder(AddMealActivity.this)
+                                            .setTitle("Enter the label name")
+                                            .setView(view)
+                                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                    alertDialogIn.dismiss();
+                                                }
+                                            })
+                                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                    String content = editText.getText().toString();
+                                                    Toast.makeText(AddMealActivity.this,content, Toast.LENGTH_SHORT).show();
+                                                    alertDialogIn.dismiss();
+                                                }
+                                            }).create();
+                                    alertDialogIn.show();
+                                    alertDialog.dismiss();
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    alertDialog.dismiss();
+                                }
+                            }).create();
+                    alertDialog.show();
+                    break;
+                case R.id.addmeal_btn_addmeal:
                     break;
             }
         }

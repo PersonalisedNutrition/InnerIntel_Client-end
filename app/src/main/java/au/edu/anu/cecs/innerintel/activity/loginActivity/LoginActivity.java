bf72704455ffcbd63fd0922af52d3ea4c433070f
Hengtong Wu <u7235221@anu.edu.au>
2022-08-31 22:36:40 +1000

    login and register debug

--- app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/LoginActivity.java
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/LoginActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.security.identity.CipherSuiteNotSupportedException;
+import android.text.InputType;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -37,26 +45,38 @@ import au.edu.anu.cecs.innerintel.utils.InformationResources;
  */
 public class LoginActivity extends AppCompatActivity
 {
-     boolean testMode = true;
+//     boolean testMode = true;
 
     //define the TAG to show the process of the programme
-    private static final String TAG = "EmailPassword";
+//    private static final String TAG = "EmailPassword";
     //
     private FirebaseAuth mAuth;
-    EditText loginEmail;
-    EditText loginPassword;
-    Button loginButton;
-    Button signUpButton;
+//    EditText loginEmail;
+//    EditText loginPassword;
+//    Button loginButton;
+//    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("Login Account");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        EditText loginEmail = (EditText)findViewById(R.id.loginEmail);
+        EditText loginPassword = (EditText)findViewById(R.id.loginPassword);
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        Button signUpButton = (Button)findViewById(R.id.signupButton);
+        TextView pswRec=(TextView)findViewById(R.id.pswRec);
 
 
-        //init the views
-        initView();
+
+//        //init the views
+//        initView();
 
         //initialize the FirebaseAuth instance.
         mAuth = FirebaseAuth.getInstance();
@@ -64,14 +84,15 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
 
-                if (testMode){
-
-                    //build user for test
-                    buildUser();
-                    login();
-
-                }else{
-                    signIn(loginEmail.getText().toString(),loginPassword.getText().toString());
+                String email=loginEmail.getText().toString().trim();
+                String password=loginPassword.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    loginEmail.setError("Invalid email");
+                    loginEmail.setFocusable(true);
+                }else {
+                    signIn(email,password);
+                    loginEmail.setText("");
+                    loginPassword.setText("");
                 }
             }
         });
@@ -83,17 +104,72 @@ public class LoginActivity extends AppCompatActivity
                 startActivity(i);
             }
         });
+
+        pswRec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pswRecover();
+            }
+        });
     }
 
 
-    /**
-     * define the initView to init views
-     */
-    public void initView(){
-        loginEmail = findViewById(R.id.loginEmail);
-        loginPassword = findViewById(R.id.userPassword);
-        loginButton = findViewById(R.id.loginButton);
-        signUpButton = findViewById(R.id.signupButton);
+//    /**
+//     * define the initView to init views
+//     */
+//    public void initView(){
+//        loginEmail = findViewById(R.id.loginEmail);
+//        loginPassword = findViewById(R.id.userPassword);
+//        loginButton = findViewById(R.id.loginButton);
+//        signUpButton = findViewById(R.id.signupButton);
+//    }
+
+    private void pswRecover() {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Password Recover");
+        LinearLayout linearLayout=new LinearLayout(this);
+        EditText emailPr=new EditText(this);
+        emailPr.setHint("Email");
+        emailPr.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+
+        linearLayout.addView(emailPr);
+        linearLayout.setPadding(20,20,20,20);
+        builder.setView(linearLayout);
+
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email=emailPr.getText().toString().trim();
+                beginRecover(email);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
+
+
+    }
+
+    private void beginRecover(String email) {
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "send Email", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
     }
 
 
@@ -103,21 +179,22 @@ public class LoginActivity extends AppCompatActivity
      * @param password password to sign in
      */
     private void signIn(String email, String password) {
-        Log.d(TAG, "signIn:" + email);
+//        Log.d(TAG, "signIn:" + email);
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
+//                            Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Login now\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
                             Intent i = new Intent(getApplicationContext(), HomepageActivity.class);
                             //need pass user id to next activity
                             startActivity(i);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                            Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -161,7 +238,11 @@ public class LoginActivity extends AppCompatActivity
 //==================================================================================
 //==================================================================================
 
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
 
 
 }
--- app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/RegisterActivity.java
+++ app/src/main/java/au/edu/anu/cecs/innerintel/activity/loginActivity/RegisterActivity.java
@@ -1,19 +1,20 @@
 package au.edu.anu.cecs.innerintel.activity.loginActivity;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -49,71 +50,132 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("Create Account");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+
 
         mAuth = FirebaseAuth.getInstance();
 
-        initView();
+//        initView();
+        EditText email_edit=(EditText) findViewById(R.id.userEmail);
+        EditText psw_edit=(EditText) findViewById(R.id.userPassword);
+        Button btSignUp=(Button) findViewById(R.id.signup);
+        TextView change_to_log=(TextView)findViewById(R.id.change_to_log);
+
+
 
         btSignUp.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("LongLogTag")
             @Override
             public void onClick(View view) {
-                if (testMode){
-                    Intent i = new Intent(getApplicationContext(),QuestionnaireActivity.class);
-                    startActivity(i);
+                String email=email_edit.getText().toString().trim();
+                String psw=psw_edit.getText().toString().trim();
 
-                }else{
-                    if (password.getText().toString()==passwordRetype.getText().toString()){
-                        createAccount(email.getText().toString(),password.getText().toString());
-                        Log.d(TAG,"Successful creation. Please return to login");
-                    }else{
-                        Toast.makeText(getApplicationContext(),"The entered passwords are inconsistent",Toast.LENGTH_LONG).show();
-                    }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    email_edit.setError("Invalid Email");
+                    email_edit.setFocusable(true);
+                }else {
+                    registerUser(email,psw);
+                    email_edit.setText("");
+                    psw_edit.setText("");
                 }
+//                if (testMode){
+//                    Intent i = new Intent(getApplicationContext(),QuestionnaireActivity.class);
+//                    startActivity(i);
+//
+//                }else{
+//                    if (password.getText().toString()==passwordRetype.getText().toString()){
+//                        createAccount(email.getText().toString(),password.getText().toString());
+//                        Log.d(TAG,"Successful creation. Please return to login");
+//                    }else{
+//                        Toast.makeText(getApplicationContext(),"The entered passwords are inconsistent",Toast.LENGTH_LONG).show();
+//                    }
+//                }
+
+            }
+        });
+
+        change_to_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    /**
-     *init views
-     */
-    private void initView(){
-        userName = findViewById(R.id.userName);
-        email = findViewById(R.id.userEmail);
-        password = findViewById(R.id.userPassword);
-        passwordRetype = findViewById(R.id.password_retype);
-        btSignUp = findViewById(R.id.signup);
-    }
+//    /**
+//     *init views
+//     */
+//    private void initView(){
+//        userName = findViewById(R.id.userName);
+//        email = findViewById(R.id.userEmail);
+//        password = findViewById(R.id.userPassword);
+////        passwordRetype = findViewById(R.id.password_retype);
+//        btSignUp = findViewById(R.id.signup);
+//    }
+
+
 
     /**
      * define the method to sign up with emails
      * @param email email to sign up with
-     * @param password password to sign up with
+     * @param psw password to sign up with
      */
-    private void createAccount(String email, String password) {
-
-        mAuth.createUserWithEmailAndPassword(email, password)
+//    private void createAccount(String email, String password) {
+//
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @SuppressLint("LongLogTag")
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "createUserWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//
+//
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//
+//
+//                        }
+//                    }
+//                });
+//
+//    }
+    private void registerUser(String email, String psw) {
+        mAuth.createUserWithEmailAndPassword(email,psw)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @SuppressLint("LongLogTag")
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+                        if (task.isSuccessful()){
+                            FirebaseUser user=mAuth.getCurrentUser();
 
 
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-
+                            Toast.makeText(getApplicationContext(), "Register now\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(),QuestionnaireActivity.class);
+                            startActivity(i);
+                        }else {
+                            Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
+
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
 }
+
